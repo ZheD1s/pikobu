ed1b4de7f2ba9d984bfee482b7669835e9cb97ba
Dias Zhetpissov <zhed1s@mail.ru>
2023-01-05 06:30:47 +0600

    login system

--- main.py
+++ main.py
@@ -15,25 +15,38 @@ def passwordHash(rawPass:str):
         passSum += ord(letter)
     return int(math.sin(passSum) * 10**16)
 
-print(passwordHash('qwerty'))
+# print(passwordHash('qwerty'))
 
 def registration():
     while True:
         login = input('Please input your username:')
-        query = "SELECT user_login FROM users_table WHERE user_login'" + login + "'"
+        query = "SELECT user_login FROM users_table WHERE user_login='" + login + "'"
         cursor.execute(query)
         if cursor.fetchall():
             print('This username is already taken. Try again')
         else:
             break
-    password = passwordHash(input('Please input your password'))
-    query = "INSERT INTO (user_login, user_password)" \
-            "VALUES('" + login + "','" + password + "')"
+    password = passwordHash(input('Please input your password:'))
+    query = "INSERT INTO users_table(user_login, user_password)" \
+            "VALUES('" + login + "','" + str(password) + "')"
     cursor.execute(query)
     connection.commit()
 
 def logIn():
-    login = input('Please input your username:')
-    password = passwordHash(input('Please input your password:'))
+    global thisUserId
+    while True:
+        login = input('Please input your username:')
+        password = passwordHash(input('Please input your password:'))
+        query = "SELECT user_id FROM users_table WHERE user_login='" + login + "' AND user_password='" + str(password) + "'"
+        cursor.execute(query)
+        thisId = cursor.fetchall()
+        if thisId:
+            print('You are logged in!')
+            thisUserId = thisId[0][0]
+            print(thisUserId)
+            return
+        else:
+            print('Username or Password is not correct. Try again!')
 
-
+# registration()
+logIn()
